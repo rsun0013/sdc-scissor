255624df309dd17032be001aa5cf250b1809d99e
Raj <rsun0013@student.monash.edu>
2022-09-26 10:07:28 +1000

    conf

--- sdc_scissor/testing_api/test_generators/ambiegen/config.py
+++ sdc_scissor/testing_api/test_generators/ambiegen/config.py
@@ -4,10 +4,10 @@ model = {
     "speed": 9,  # parameter for the simplified car model
     "map_size": 200,
     "steer_ang": 12,  # a parameter for the simplified car model
-    "min_len": 28,  # minimal possible distance in meters
-    "max_len": 30,  # maximal possible disance to go straight in meters
-    "min_angle": 2,  # minimal angle of rotation in degrees
-    "max_angle": 20,  # maximal angle of rotation in degrees
+    "min_len": 10,  # minimal possible distance in meters
+    "max_len": 20,  # maximal possible disance to go straight in meters
+    "min_angle": 10,  # minimal angle of rotation in degrees
+    "max_angle": 70,  # maximal angle of rotation in degrees
 }
 
 files = {"ga_archive": ".\\GA_archive\\", "tc_img": ".\\TC_img\\", "tc_file": ".\\TC_file\\"}
